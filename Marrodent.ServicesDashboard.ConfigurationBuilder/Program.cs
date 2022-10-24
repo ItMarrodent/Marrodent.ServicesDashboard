27252207a0c65ce15fb9029976cbac74ce8cb623
Dawid Grzeszuk <dawid.grzeszuk@bst.com.pl>
2022-10-24 08:48:53 +0200

    Marrodent.ServicesDashboard.ConfigurationBuilder

---
+++ Marrodent.ServicesDashboard.ConfigurationBuilder/Program.cs
@@ -0,0 +1,15 @@
+﻿//Usings
+using Marrodent.ServicesDashboard.Models.Models;
+using Newtonsoft.Json;
+
+//IIS
+ICollection<IISApp> iisApps = new List<IISApp>();
+File.WriteAllText(@"json\iis.json", JsonConvert.SerializeObject(iisApps));
+
+//Services
+ICollection<WindowsServiceApp> windowsServiceApps = new List<WindowsServiceApp>();
+File.WriteAllText(@"json\services.json", JsonConvert.SerializeObject(windowsServiceApps));
+
+//Scheduler
+ICollection<WindowsTaskSchedulerApp> windowsTaskSchedulerApps = new List<WindowsTaskSchedulerApp>();
+File.WriteAllText(@"json\tasks.json", JsonConvert.SerializeObject(windowsTaskSchedulerApps));
