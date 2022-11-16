e504cf3dc5b399ad357df49955179202ee8e84ea
dawid.grzeszuk <dawid.grzeszuk@marrodent.pl>
2022-11-16 09:25:37 +0100

    Terminal

--- Marrodent.ServicesDashboard/Models/Models/Terminal.cs
+++ Marrodent.ServicesDashboard/Models/Models/Terminal.cs
@@ -5,4 +5,5 @@ public sealed class Terminal
     public string Address { get; set; }
     public double TotalRam { get; set; }
     public double AvailableRam { get; set; }
+    public string UsedRam => $"{Math.Round((TotalRam - AvailableRam)/TotalRam * 100, 2)} %";
 }
